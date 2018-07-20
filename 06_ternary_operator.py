7e79eb7787db8bf612e7929750a9077426e59b84
linbo0518 <linbo0518@gmail.com>
2018-07-20 12:23:44 +0800

    add ternary operator

---
+++ 06_ternary_operator.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    is_fat = True
+    state = 'fat' if is_fat else 'not fat'
+    print(state)
+
+    fat = True
+    fitness = ("skinny", "fat")[fat]
+    print(fitness)
+
+    condition = True
+    print(2 if condition else 1 / 0)
+
+    try:
+        print((1 / 0, 2)[condition])
+    except Exception as e:
+        print(exit)
