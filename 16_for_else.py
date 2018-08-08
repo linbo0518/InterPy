01f97d3b9b9369b320c00d23f42db881bee153a4
linbo0518 <linbo0518@gmail.com>
2018-08-08 17:38:58 +0800

    add for else

---
+++ 16_for_else.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+
+    for n in range(2, 10):
+        for x in range(2, n):
+            if n % x == 0:
+                print(n, 'equals', x, '*', n / x)
+                break
+        else:
+            print(n, 'is a prime number')
